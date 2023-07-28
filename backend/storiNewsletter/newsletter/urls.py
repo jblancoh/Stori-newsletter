12622fdbc811052157a8948a2905b8524faea9f0
jonathan <zlatanjon@hotmail.com>
2023-07-27 23:38:41 -0600

    feat(send_email): Add email template; Modify unsubscribe method;

--- backend/storiNewsletter/newsletter/urls.py
+++ backend/storiNewsletter/newsletter/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('upload_newsletter/', views.upload_newsletter, name='upload_newsletter'),
     path('send_newsletter/', views.send_newsletter, name='send_newsletter'),
-    path('unsubscribe/<str:email>/', views.unsubscribe, name='unsubscribe'),
+    path('unsubscribe/<str:email>/<int:newsletter_id>/', views.unsubscribe, name='unsubscribe'),
     path('add_subscriber/', views.add_subscriber, name='add_subscriber'),
 ]
