13b2a2c9252c8c00575d39a3e5644459a7913205
jonathan <zlatanjon@hotmail.com>
2023-07-27 22:36:03 -0600

    fix(gitignore): Remove __pycache__

--- backend/storiNewsletter/newsletter/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Subscriber, Newsletter
-
-admin.site.register(Subscriber)
-admin.site.register(Newsletter)
--- backend/storiNewsletter/newsletter/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('upload_newsletter/', views.upload_newsletter, name='upload_newsletter'),
-    path('send_newsletter/', views.send_newsletter, name='send_newsletter'),
-    path('unsubscribe/<str:email>/', views.unsubscribe, name='unsubscribe'),
-    path('add_subscriber/', views.add_subscriber, name='add_subscriber'),
-]
