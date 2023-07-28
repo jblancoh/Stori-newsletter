33d4b41e38ec890d09b275657f46fa2d2cdc5110
jonathan <zlatanjon@hotmail.com>
2023-07-28 17:27:34 -0600

    feta(django-cors-headers): add django-cors-headers library

--- backend/storiNewsletter/newsletter/admin.py
+++ backend/storiNewsletter/newsletter/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Subscriber, Newsletter
+from .models import Subscriber, Newsletter,CategoryNewsletter
 
 admin.site.register(Subscriber)
 admin.site.register(Newsletter)
+admin.site.register(CategoryNewsletter)
--- backend/storiNewsletter/newsletter/views.py
+++ backend/storiNewsletter/newsletter/views.py
@@ -1,4 +1,4 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Newsletter, Subscriber
